3046ca34b6dd4e25060c9e99cbc05c0d0af60fb0
VolchkovIlia <113889753+VolchkovIlia@users.noreply.github.com>
2023-03-01 23:43:14 +0300

    Add files via upload

---
+++ CustomArray/CustomArray/CustomArray.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomArray
+{
+    public class CustomArray<TItem>
+    {
+        private TItem[] items;
+
+        public CustomArray()
+        {
+            items = new TItem[0];
+        }
+
+        public int Length => items.Length;
+
+        public void Add(TItem item)
+        {
+            Array.Resize(ref items, items.Length + 1);
+            items[items.Length - 1] = item;
+        }
+
+        public void Delete(int index)
+        {
+            if (index < 0 || index >= items.Length)
+            {
+                throw new IndexOutOfRangeException("Индекс вне диапазона");
+            }
+
+            TItem[] newItems = new TItem[items.Length - 1];
+
+            for (int i = 0, j = 0; i < items.Length; i++)
+            {
+                if (i == index)
+                {
+                    continue;
+                }
+
+                newItems[j++] = items[i];
+            }
+
+            items = newItems;
+        }
+
+        public TItem FindFirst(Func<TItem, bool> filter)
+        {
+            return items.FirstOrDefault(filter);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(string.Join(", ", items));
+        }
+
+        public void Swap(int index1, int index2)
+        {
+            if (index1 < 0 || index1 >= items.Length || index2 < 0 || index2 >= items.Length)
+            {
+                throw new IndexOutOfRangeException("Индекс вне диапазона");
+            }
+
+            TItem temp = items[index1];
+            items[index1] = items[index2];
+            items[index2] = temp;
+        }
+    }
+}
---
+++ CustomArray/CustomArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using CustomArray;
+
+var array = new CustomArray<int>();
+
+array.Add(1);
+array.Add(2);
+array.Add(3);
+array.Add(4);
+array.Add(5);
+
+Console.WriteLine("Инициализация массива :");
+array.Print();
+
+array.Delete(2);
+
+Console.WriteLine("Массив после удаления индекса 2:");
+array.Print();
+
+int first = array.FindFirst(x => x % 2 == 0);
+Console.WriteLine($"Первое чётное число: {first}");
+
+array.Swap(0, 3);
+
+Console.WriteLine("Массив после замены индексов 0 и 3:");
+array.Print();
+
+Console.ReadKey();
